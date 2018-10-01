4359120dac86b4d89aa27d0dc19f299854b1e5de
„Sebastian <bastigeiger@gmail.com>
2018-10-01 23:22:11 +0200

    erweitert

--- 05-arrow-functions/src/app.ts
+++ 05-arrow-functions/src/app.ts
@@ -1,3 +1,15 @@
-const pizzas = [{ name: 'Pepperoni', toppings: ['pepperoni'] }];
+const pizzas = [
+  { name: 'Pepperoni', toppings: ['pepperoni'] },
+  { name: 'Salami', toppings: ['salami'] },
+  { name: 'Hawai', toppings: ['ananas, schinken'] },
+];
 
-const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
+const mappedPizzas = pizzas.map((pizza, index) => {
+  const pName = index + ' ' + pizza.name.toUpperCase();
+  return pName;
+});
+
+const mappedPizzas_small = pizzas.map(pizza => pizza.name);
+
+console.log(mappedPizzas);
+console.log(mappedPizzas_small);
